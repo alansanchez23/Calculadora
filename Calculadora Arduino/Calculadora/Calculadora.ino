b6552cbd7c852fddf300a14e625e40d5bc4116e6
demiendops <86987778+demiendops@users.noreply.github.com>
2021-07-05 17:35:17 -0500

    Update Calculadora.ino
    Agregué la función de multiplicación en esta ultima actualización.

--- Calculadora Arduino/Calculadora/Calculadora.ino
+++ Calculadora Arduino/Calculadora/Calculadora.ino
@@ -1,6 +1,7 @@
 
 long suma(int,int);
 long resta(int,int);
+long multi(int,int);
 
 void setup() {
   Serial.println(9600);
@@ -11,24 +12,30 @@ void loop() {
 int a = 10, b = 5;
 long c = 0;
 long d=0;
+long e=0;
 
 ///Llamado de las funciones 
 c = suma(a,b);
 d = resta(a,b);
+e = multi(a,b);
 
 ///Impresion de los resultados
 Serial.println("El resultado de la suma es: "+c);
 Serial.println("El resultado de la resta es: "+d);
-
+Serial.println("El resultado de la multiplicacion es: "+e);
 }
 
-long suma(int a,int b){   ///Esta es la funcion de alan sanchez suma
+long suma(int a,int b){   ///Esta es la funcion de alan sanchez SUMA
 long c = a+b; 
 return c;
 }
 
-{
 long resta(int a,int b){   ///Esta es la funcion de Miguel Gomez RESTA
 long c = a-b; 
 return c;
+}
+
+long multi(int a,int b){  //Esta es la función de Jorge Cuevas MULTIPLICACIÓN
+  long e = a*b;
+  return e;
 }
