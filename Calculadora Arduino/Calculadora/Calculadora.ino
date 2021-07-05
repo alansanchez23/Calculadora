423d92c2770d98732b170ed7ca1b44c64ff82805
MiGo1403 <85647005+MiGo1403@users.noreply.github.com>
2021-07-05 15:40:54 -0500

    Update Calculadora.ino
    Agregue la funcion para la resta dentro del void loop.

--- Calculadora Arduino/Calculadora/Calculadora.ino
+++ Calculadora Arduino/Calculadora/Calculadora.ino
@@ -1,5 +1,6 @@
 
 long suma(int,int);
+long resta(int,int);
 
 void setup() {
   Serial.println(9600);
@@ -10,6 +11,8 @@ void loop() {
 int a = 10, b = 5;
 long c = 0;
 c = suma(a,b);
+long d=0;
+d = resta(a,b);
 
 Serial.println("El resultado de la operaciones es: "+c);
 
@@ -17,5 +20,11 @@ Serial.println("El resultado de la operaciones es: "+c);
 
 long suma(int a,int b){   ///Esta es la funcion de alan sanchez suma
 long c = a+b; 
+return c;
+}
+
+{
+long resta(int a,int b){   ///Esta es la funcion de Miguel Gomez RESTA
+long c = a-b; 
 return c;
 }
