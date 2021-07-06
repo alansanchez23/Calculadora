3dc6c4e8c6ab128234636fceb582561f155b89ed
bautista98 <86986729+bautista98@users.noreply.github.com>
2021-07-05 19:42:17 -0500

    Update Calculadora.ino

--- Calculadora Arduino/Calculadora/Calculadora.ino
+++ Calculadora Arduino/Calculadora/Calculadora.ino
@@ -2,6 +2,7 @@
 long suma(int,int);
 long resta(int,int);
 long multi(int,int);
+long division(int,int);
 
 void setup() {
   Serial.println(9600);
@@ -13,16 +14,19 @@ int a = 10, b = 5;
 long c = 0;
 long d=0;
 long e=0;
+long f=0;
 
 ///Llamado de las funciones 
 c = suma(a,b);
 d = resta(a,b);
 e = multi(a,b);
+f = division(a,b);
 
 ///Impresion de los resultados
 Serial.println("El resultado de la suma es: "+c);
 Serial.println("El resultado de la resta es: "+d);
 Serial.println("El resultado de la multiplicacion es: "+e);
+Serial.println)"El resultado de la division es: "+f);
 }
 
 long suma(int a,int b){   ///Esta es la funcion de alan sanchez SUMA
@@ -38,4 +42,9 @@ return c;
 long multi(int a,int b){  //Esta es la función de Jorge Cuevas MULTIPLICACIÓN
   long e = a*b;
   return e;
+}
+
+long division(int a, int b){ //Esta es la función de Bautista División
+  long f = a/b;
+  return f;
 }
